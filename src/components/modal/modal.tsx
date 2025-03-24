2855641e09b5f5ddb5daba408a3192349661e161
Alexkucepalov <kucepalov134@mail.ru>
2025-03-24 14:50:01 +0300

    add DragIcon, delete propTypes, add tag main and section

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import s from './modal.module.scss';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import PropTypes from 'prop-types';
 
 interface ModalProps {
     children: React.ReactNode;
@@ -39,12 +38,3 @@ export const Modal: React.FC<ModalProps> = ({ children, onClose }) => {
         document.getElementById('modals') as HTMLElement
     );
 };
-
-Modal.propTypes = {
-    children: PropTypes.node.isRequired,
-    onClose: PropTypes.func.isRequired
-};
-
-ModalOverlay.propTypes = {
-    onClose: PropTypes.func.isRequired
-}; 
